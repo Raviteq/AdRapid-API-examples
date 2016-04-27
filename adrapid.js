4557ed3afae6d6b684314515829c62792b7726f5
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-04-27 13:07:30 +0200

    Init

---
+++ adrapid.js
@@ -0,0 +1,69 @@
+var AdRapid = function(params) {
+
+	// initialize
+	(function init() {
+		this.api_url 					= params.api_url || 'http://api.adrapid.com/',
+		this.api_key_public 	= params.api_key || 'myAPIkey';
+		this.debug 						= params.debug || false,
+		this.inspectors 			= [];
+	})();
+
+	// public methods
+	// ---------------------------------------------------
+
+	// get templates
+	this.templates = function() {
+		return this.api_get('templates');
+	},
+
+	// get rules for template
+	this.rules = function(templateId) {
+		return this.api_get('templates/' + templateId);
+	},
+
+	// send order
+	this.sendOrder = function(data) {
+		return this.api_get('orders', false, data);
+	},
+
+	// get order
+	this.getOrder = function(orderID) {
+		return this.api_get('orders/' + orderID);
+	},
+
+	// get item preview
+	this.getPreview = function(itemID) {
+		return this.api_get('get_item_content/' + itemID);
+	},
+
+	// public api helper 
+	this.api_get = function(method, params, data) {
+		if(data) params = data;
+		if(!params) params = {}
+
+		log(params, 'Called <strong>' + method + '</strong> method with data:');
+	  
+	  // Return a new promise.
+	  return new Promise(function(resolve, reject) {
+		  $.ajax({  
+				type: (data) ? 'POST' : 'GET',
+				url: this.api_url + method,
+				data: params,
+				headers: { 'Authorization': this.api_key_public }, // cant do this locally
+				success: function(response) {
+					response = jQuery.parseJSON(response);
+					log(response, 'Got result of <strong>' + method + '</strong> method');
+	        resolve(response);
+				},
+				error: function(status) {
+	        reject(Error(status));
+				}
+			});
+	  });
+	}
+
+	// a log helper, may be extended to do something else
+	this.log = function(content, title) {
+		console.log(content);
+	}
+};
