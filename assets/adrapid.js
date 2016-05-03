4e1b3344338c369ff121f041a61f7da225249c0e
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-05-03 13:18:19 +0200

    Simplified demo implementations by moving some logic to adrapid wrapper

--- assets/adrapid.js
+++ assets/adrapid.js
@@ -6,6 +6,12 @@ var AdRapid = function(params) {
     this.api_key_public   = params.api_key || 'myAPIkey',
     this.debug            = params.debug || false,
     this.inspectors       = [];
+    // a log helper, may be extended to do something else
+    if(!this.log) this.log = function(content, title) {
+      if(title) console.log(title + ':');
+      console.log(content);
+    }
+    var log = this.log;
   })();
 
   // public methods
@@ -41,7 +47,7 @@ var AdRapid = function(params) {
     if(data) params = data;
     if(!params) params = {}
 
-    if(data && debug && typeof(log) == "function") log(data, 'POST -> ' + method);
+      if(data && debug && typeof(log) == "function") log(data, 'POST -> ' + method);
 
     // Return a new promise.
     return new Promise(function(resolve, reject) {
@@ -63,9 +69,109 @@ var AdRapid = function(params) {
     });
   }
 
-  // a log helper, may be extended to do something else
-  this.log = function(content, title) {
-    if(title) console.log(title + ':');
-    console.log(content);
+  
+
+
+
+
+  // additional frontend methods
+  // ---------------------------------------------------
+
+  // the watch function
+  this.watch = function(params) {
+    var func = function(){};
+
+    // setup defaults
+    params.method         = params.method || 'watch_order';
+    params.update         = params.update || func;
+    params.complete       = params.complete || func;
+    params.init           = params.init || function(){ console.log('Init this watcher'); };
+    params.init();
+
+    var evtSource = new EventSource(api_url + params.method + '/' + params.order_id);
+
+    // show items as they are completed
+    evtSource.addEventListener("item_complete", function(e) {
+      params.update(JSON.parse(e.data));
+    }, false);
+
+    // when order is completed
+    evtSource.addEventListener('order_complete', function(e) {
+      params.complete(JSON.parse(e.data));      
+      evtSource.close(); // remove the eventsource
+    }, false);
+
   }
+
+  this.createForm = function(data, template) {
+    var form = '';
+    
+    // create formats dropdown
+    form += '<br> <label for="formats">Banner format</label> <br><select name="formats"><br>';
+    for(var i = 0; i < data.formats.length; ++i) form += '<option value="banner_' + data.formats[i] + ':google_adwords">' + data.formats[i] + '</option>';
+      form += '</select> <br><br>';
+    
+    // create form fields
+    for(var i = 0; i < data.fields.length; ++i) {
+      if(data.fields[i]['name'].indexOf('img') > -1 || data.fields[i]['name'].indexOf('color') > -1) {} else {
+        form += '<label for="' + data.fields[i]['name'] + '">' + data.fields[i]['label'] + '</label> <br>' + '<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '"><br>';
+      }
+    }
+
+    // create form images
+    form += '<div class="images">';
+    for(var i = 0; i < data.fields.length; ++i) {
+      if(data.fields[i]['type'] == 'image') {
+        data.fields[i]['default'] = data.fields[i]['default'].replace('10.0.0.150', 'test.adrapid.com');
+
+        form += 
+        '<div class="image">' + '<label>' + data.fields[i]['label'] + '</label>' +
+        '<img src="' + data.fields[i]['default'] + '" width="150" /><br>' +
+        '<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '"></div>';
+      }
+    }
+
+    form += '</div><br><div class="colors">';
+
+    // create form colors
+    for(var i = 0; i < data.fields.length; ++i) {
+      if(data.fields[i]['type'] == 'color') {
+        form += '<label for="' + data.fields[i]['name'] + '">' + data.fields[i]['label'] + '</label> <br>' + 
+        '<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '" class="colorPicker"><br>';
+      }
+    }
+
+    form += '</div>';
+
+    // add colorpickers
+    // setTimeout(function() { $('.colorPicker').minicolors(); }, 0);
+
+    // set form content
+    // TODO: add support for complete callback here
+    $('#form-content').html(form);
+    $('#the_form button').show();
+    $('.form_placeholder').hide();
+  }
+
+
+
+  // serialize to object helper
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    
+    return o;
+  };
+
 };
--- simple/app.js
+++ simple/app.js
@@ -1,235 +1,137 @@
-// AdRapid API demo app
-
+// AdRapid API sample implementation
 
 // config
 var api_url = 'http://test.adrapid.com/api/';
 var api_key = '6271f323ff24875b74569ebc76eafa7c8ce0aa85'; // apidemo@dev
-							
-
-// log helper
-function log(content, title) {
-	if(!content) return false;
-
-	// if content is object, implement code inspector
-	if(typeof content === 'object') {
-		var logId = 'log-' + Date.now();
-		$("#log .content").prepend("<div class=\"log-message\" id=\"" + logId + "\"></div>")
-
-		inspectors.push(new InspectorJSON({
-			element: logId,
-			json: content,
-		}));
-
-		if(title) $('#log .content').prepend('<div class="log-message">' + title + ':</div>');
-	} else {
-		$('#log .content').prepend('<div class="log-message">' + content + '</div>');
-	}
-}
-
 
 // initialize AdRapid
 var adrapid = new AdRapid({
-	api_key: 	api_key,
-	api_url: 	api_url,
-	debug:  	true,
+  api_key:  api_key,
+  api_url:  api_url,
+  debug:    true,
 });
 
-// add our custom log function to the AdRapid object
-adrapid.log = log;
+// extend adrapid with a custom log function
+function log(content, title) {
+  if(!content) return false;
 
+  // if content is object, implement code inspector
+  if(typeof content === 'object') {
+    var logId = 'log-' + Date.now();
+    $("#log .content").prepend("<div class=\"log-message\" id=\"" + logId + "\"></div>")
+
+    inspectors.push(new InspectorJSON({
+      element: logId,
+      json: content,
+    }));
+
+    if(title) $('#log .content').prepend('<div class="log-message">' + title + ':</div>');
+  } else {
+    $('#log .content').prepend('<div class="log-message">' + content + '</div>');
+  }
+}
+
+adrapid.log = log;
 
 // define our app
 $(function() {
 
-	// environment vars
-	var projects = [], inspectors = [];
-			
-	// initialize the app
-	(function init() {
+  // environment vars
+  var projects = [], inspectors = [];
+      
+  // initialize the app
+  (function init() {
 
-		// hide extra content
-		$('#the_form button').hide();
-		$('#form').hide();
+    // hide extra content
+    $('#the_form button').hide();
+    $('#form').hide();
 
-		// get list of templates
-		adrapid.templates().then(function(templates) {
+    // get list of templates
+    adrapid.templates().then(function(templates) {
 
-		  // initiate templates
-		  $.each(templates, function(i, data) {
+      // initiate templates
+      $.each(templates, function(i, data) {
 
-				// add project API key to array of available projects
-				projects[data.identifier] = data.templateId;
+        // add project API key to array of available projects
+        projects[data.identifier] = data.templateId;
 
-				// append template to UI
-				$('#templates').append('<div class="template" name="' + data.identifier + '"><img src="' + data.thumbnail + '" style="width: 100%; " /></div>');			
-			});
+        // append template to UI
+        $('#templates').append('<div class="template" name="' + data.identifier + '"><img src="' + data.thumbnail + '" style="width: 100%; " /></div>');      
+      });
 
-			// bind template click actions
-			bind_buttons();
+      // bind template click actions
+      bind_buttons();
 
-			$('.loader').fadeOut();
+      $('.loader').fadeOut();
 
-		}, function(error) {
-		  console.log(error);
-		});
+    }, function(error) {
+      console.log(error);
+    });
 
-	})();
+  })();
 
-	// handle button bindings
-	function bind_buttons() {
+  // handle button bindings
+  function bind_buttons() {
 
-		// when a template is clicked
-		$('.template').click(function(e) {
-			$('.template').removeClass('active');
-			$(this).toggleClass('active');
-			loadForm(projects[$(this).attr('name')]);
-		});
+    // when a template is clicked, load the form
+    $('.template').click(function(e) {
+      $('.template').removeClass('active');
+      $(this).toggleClass('active');
+      loadForm(projects[$(this).attr('name')]);
+    });
 
-		// when the form is submitted
-		$('.send').click(function(e) {
-			e.preventDefault();
-			
-			var selectedTemplate 	= $('.active').attr('name'),
-					order 						= $('form#the_form').serializeObject();
-					order.templateId 	= projects[selectedTemplate], // from our api-keys config
-			
-			// send the order
-			adrapid.sendOrder(order).then(function(result) {
-				watch_order(result.order_id);
-			});
+    // when the form is submitted, handle sending the order to AdRapid
+    $('.send').click(function(e) {
+      e.preventDefault();
+      send_order();
+    });
 
-		});
+    // back button
+    $('#back').click(function(event) {
+      event.preventDefault();
+      $('#form').hide();
+      $('#templates').show();
+    });
 
-		// back button
-		$('#back').click(function(event) {
-			event.preventDefault();
-			$('#form').hide();
-			$('#templates').show();
-		});
+  }
 
-	}
+  function send_order() {
+    var selectedTemplate  = $('.active').attr('name'),
+        order             = $('form#the_form').serializeObject();
+        order.templateId  = projects[selectedTemplate], // from our api-keys config
+    
+    // send the order
+    adrapid.sendOrder(order).then(function(result) {
+      watch_order(result.order_id);
+    });
+  }
 
+  // watch an order for events
+  function watch_order(order_id) {
+    adrapid.watch({
+      order_id: order_id,
+      init: function() {
+        $('.loader').fadeIn();
+      },
+      update: function(data) {
+        adrapid.getPreview(data.id).then(function(res) {
+          $('#results').html(res.preview);
+        });
+      },
+      complete: function(data) {
+        $('.loader').fadeOut();
+      },
+    });
+  }
 
-	// load form helper 
-	function loadForm(template) {
-		adrapid.rules(template).then(function(rules) {
-			createForm(rules, template);
-		});
+  // load form helper 
+  function loadForm(template) {
+    adrapid.rules(template).then(function(rules) {
+      adrapid.createForm(rules, template);
+    });
 
-		$('#templates').hide();
-		$('#form').show();
-	}
-
-	// watch order for changes, display order items as they are completed 
-	function watch_order(order_id) {
-		$('.loader').fadeIn();
-
-		// initialize EventSource for order
-		var evt_url 	= api_url + "watch_order/" + order_id,
-				evtSource = new EventSource(evt_url);
-		
-		// show items as they are completed
-		evtSource.addEventListener("item_complete", function(e) {
-			var data = JSON.parse(e.data);
-			
-			// show a preview of the order item
-			adrapid.getPreview(data.id).then(function(res) {
-				$('#results').html(res.preview);
-			});
-
-		}, false);
-
-    // when order is completed
-    evtSource.addEventListener('order_complete', function(e) {
-    	var data = JSON.parse(e.data);
-    	// log(data, 'Got order complete event');
-			$('.loader').fadeOut();
-
-			// get order data
-			// adrapid.getOrder(order_id).then(function(order){
-			// 	log(order, 'Order result data');
-			// });
-
-			// remove the eventsource
-			evtSource.close();
-
-		}, false);
-
-	}
-
-	// form helpers
-	function createForm(data, template) {
-		var form = '';
-				// form += '<h3>' + template + '</h3>';
-		
-		// create formats dropdown
-		form += '<br> <label for="formats">Banner format</label> <br><select name="formats"><br>';
-		for(var i = 0; i < data.formats.length; ++i) form += '<option value="banner_' + data.formats[i] + ':google_adwords">' + data.formats[i] + '</option>';
-		form += '</select> <br><br>';
-		
-		// create form fields
-		for(var i = 0; i < data.fields.length; ++i) {
-			if(data.fields[i]['name'].indexOf('img') > -1 || data.fields[i]['name'].indexOf('color') > -1) {} else {
-				form += '<label for="' + data.fields[i]['name'] + '">' + data.fields[i]['label'] + '</label> <br>' + '<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '"><br>';
-			}
-		}
-
-		// create form images
-		form += '<div class="images">';
-		for(var i = 0; i < data.fields.length; ++i) {
-			if(data.fields[i]['type'] == 'image') {
-				data.fields[i]['default'] = data.fields[i]['default'].replace('10.0.0.150', 'test.adrapid.com');
-
-				form += 
-					'<div class="image">' + '<label>' + data.fields[i]['label'] + '</label>' +
-					'<img src="' + data.fields[i]['default'] + '" width="150" /><br>' +
-					'<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '"></div>';
-			}
-		}
-
-		form += '</div><br><div class="colors">';
-
-		// create form colors
-		for(var i = 0; i < data.fields.length; ++i) {
-			if(data.fields[i]['type'] == 'color') {
-				form += '<label for="' + data.fields[i]['name'] + '">' + data.fields[i]['label'] + '</label> <br>' + 
-								'<input name="' + data.fields[i]['name'] + '" value="' + data.fields[i]['default'] + '" class="colorPicker"><br>';
-			}
-		}
-
-		form += '</div>';
-
-
-
-
-		// add colorpickers
-		// setTimeout(function() { $('.colorPicker').minicolors(); }, 0);
-
-		// set form content
-		$('#form-content').html(form);
-		$('#the_form button').show();
-		$('.form_placeholder').hide();
-	}
+    $('#templates').hide();
+    $('#form').show();
+  }
 
 });
-
-
-
-// serialize to object helper
-$.fn.serializeObject = function() {
-  var o = {};
-  var a = this.serializeArray();
-
-  $.each(a, function() {
-      if (o[this.name] !== undefined) {
-          if (!o[this.name].push) {
-              o[this.name] = [o[this.name]];
-          }
-          o[this.name].push(this.value || '');
-      } else {
-          o[this.name] = this.value || '';
-      }
-  });
-  
-  return o;
-};
