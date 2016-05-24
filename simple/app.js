298ac0a61f71c6c35ac84676ca896a63ffdafd0b
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-05-24 18:16:05 +0200

    Updated & simplified `simple` example implementation

--- simple/app.js
+++ simple/app.js
@@ -1,99 +1,70 @@
 // AdRapid API sample implementation
-
-// initialize AdRapid
-// var adrapid = new AdRapid('6271f323ff24875b74569ebc76eafa7c8ce0aa85');
+// Documentation available at http://raviteq.github.io/adrapid-api/
+var projects = [];
 var adrapid = new AdRapid({
   api_key: '6271f323ff24875b74569ebc76eafa7c8ce0aa85',
-  log: helpers.log,
+  log: helpers.log // add a custom log function
 });
 
+// initialize
+adrapid.templates().then(setup_templates);
+$('#the_form button, #form').hide();
 
-adrapid.log = helpers.log;
+function setup_templates(templates) {
+  $.each(templates, function(i, data) {
+    projects[data.identifier] = data.templateId; // add project API key to array of available projects
+    $('#templates').append('<div class="template" name="' + data.identifier + '"><img src="' + data.thumbnail + '" style="width: 100%; " /></div>'); // append template to UI
+  });
 
+  bind_buttons();
+}
 
-// define our app
-$(function() {
+function bind_buttons() {
 
-  // environment vars
-  var projects = [], inspectors = [];
-      
-  // initialize
-  (function init() {
+  // when a template is clicked, load the form
+  $('.template').click(function(e) {
+    $('.template').removeClass('active');
+    $(this).toggleClass('active');
+    loadForm(projects[$(this).attr('name')]);
+  });
 
-    // hide some content
-    $('#the_form button').hide();
-    $('#form').hide();
+  // when the form is submitted, handle sending the order to AdRapid
+  $('.send').click(function(event) {
+    event.preventDefault();
+    send_order();
+  });
 
-    // get list of templates
-    adrapid.templates().then(setup_templates);
+  // handle back button
+  $('.back').click(function(event) {
+    event.preventDefault();
+    $('#form, #templates').toggle();
+  });
 
-  })();
+}
 
-  // initiate templates
-  function setup_templates(templates) {
+function send_order() {
+  var order             = $('form#the_form').serializeObject();
+      order.templateId  = projects[$('.active').attr('name')], // from our api-keys config
+  
+  adrapid.sendOrder(order).then(helpers.watchOrder);
+}
 
-    // setup templates
-    $.each(templates, function(i, data) {
-      projects[data.identifier] = data.templateId; // add project API key to array of available projects
-      $('#templates').append('<div class="template" name="' + data.identifier + '"><img src="' + data.thumbnail + '" style="width: 100%; " /></div>'); // append template to UI
+// load form helper 
+function loadForm(template) {
+  adrapid.rules(template).then(function(rules) {
+    helpers.buildForm(rules, template, {
+      selector: '#form-content',
+      before: function() {
+        $('#the_form button, .form_placeholder').toggle();
+      },
+      complete: function(data) {
+        // $('#the_form button, .form_placeholder').toggle();
+        $('.loader').fadeOut();
+        $('#the_form button').show();
+        $('.form_placeholder').hide();
+      }
     });
+  });
 
-    // bind template click actions
-    bind_buttons();
-    $('.loader').fadeOut();
-  }
-
-  // handle button bindings
-  function bind_buttons() {
-
-    // when a template is clicked, load the form
-    $('.template').click(function(e) {
-      $('.template').removeClass('active');
-      $(this).toggleClass('active');
-      loadForm(projects[$(this).attr('name')]);
-    });
-
-    // when the form is submitted, handle sending the order to AdRapid
-    $('.send').click(function(e) {
-      e.preventDefault();
-      send_order();
-    });
-
-    // handle back button
-    $('.back').click(function(event) {
-      event.preventDefault();
-      $('#form').hide();
-      $('#templates').show();
-    });
-  }
-
-  function send_order() {
-    var order             = $('form#the_form').serializeObject();
-        order.templateId  = projects[$('.active').attr('name')], // from our api-keys config
-    
-    // send the order
-    adrapid.sendOrder(order).then(helpers.watchOrder);
-  }
-
-  // load form helper 
-  function loadForm(template) {
-    adrapid.rules(template).then(function(rules) {
-      helpers.buildForm(rules, template, {
-        // rules: rules,
-        selector: '#form-content',
-        before: function() {
-          $('#the_form button').show();
-          $('.form_placeholder').hide();
-        },
-        complete: function(data) {
-          $('#the_form button').show();
-          $('.form_placeholder').hide();
-        }
-      });
-    });
-
-    $('#templates').hide();
-    $('#form').show();
-  }
-
-});
+  $('#templates, #form').toggle();
+}
