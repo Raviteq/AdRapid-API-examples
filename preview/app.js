ee9e8bcae60916d41bf2bcf0fc725b0ef61d6b8c
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-07-28 01:02:55 +0200

    Updated and simplified `preview` example

--- preview/app.js
+++ preview/app.js
@@ -1,56 +1,13 @@
-  
-  var format = '970x250',
-	    projects = [],
-	    adrapid = new AdRapid('6271f323ff24875b74569ebc76eafa7c8ce0aa85');
+var api_key = '6271f323ff24875b74569ebc76eafa7c8ce0aa85';
+var template_key = '1dbc77d334cce79b2d8eb6ff96b9674995787df6';
+var adrapid = new AdRapid(api_key);
 
-  // app initialization
-  adrapid.templates()
-    .then(filter_templates)
-    .then(setup)
-    .then(click_actions);
+$('#load').click(function(event) {
+  $('#target h3').text('Loading...');   // set loading text
+  $('#form, #templates').toggle();      // show the form
+  helpers.getForm(template_key);               // get the form for the template
+  helpers.getLivePreview(template_key); // get the live preview, bind form events
+  console.log('Load preview');
+});
 
-
-  // helper functions
-
-  function filter_templates(templates) {
-    return templates.filter(function(template) {
-      return template.group == 'offer';
-    });
-  }
-
-  function setup(templates) {
-    $.each(templates, function(i, data) {
-      projects[data.identifier] = data.templateId;
-      $('#templates').append('<div class="template" name="' + data.identifier + '"><img src="' + data.thumbnail + '" style="width: 100%; " /></div>');
-    });
-    return templates;
-  }
-
-  function click_actions() {
-    $('.template').click(function(event) {
-      $('#form, #templates').toggle();
-      $('#target').html('<h3>Loading..</h3>');
-      var template = $(this).attr('name');
-      getForm(template);
-      getLivePreview(projects[template]);
-    });
-  }
-
-  function getLivePreview(templateId) {
-    helpers.loadEdge(function() { // make sure Adobe Edge runtime is loaded first
-      adrapid.getPreviewHtml5(templateId) // get the animation content
-        .then(helpers.appendEdgeAnimation) // append the banner to the page
-        .then(helpers.previewHelper) // bind form events
-        .then(helpers.addUploadHelpers); // add file uploads to form
-    });
-  }
-
-  function getForm(template) {
-    adrapid.rules(template).then(function(rules) { // get the rules for the template
-      helpers.buildForm(rules, false, { // load the form for the template
-        formats: false, // dont show formats dropdown
-      });
-    });
-  }
-
-  $('#form').hide();
+$('#form').hide(); // hide the form initially
